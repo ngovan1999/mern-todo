e9688ce1bf7e35f239438bc148e9afdb03a61a06
VanNGo <ngotrongvan1999@gmail.com>
2022-05-19 14:09:04 +0700

    deyploy docker

--- server/configdb/db.js
+++ server/configdb/db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/mern123", {
+const connectDB = () => {
+  mongoose.Promise = global.Promise;
+  mongoose
+    .connect("mongodb://mongo:27017/mern123", {
       useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("success");
+    })
+    .catch((err) => {
+      console.error("err hihi ", err);
+      process.exit(1);
     });
-
-    console.log("MongoDB is Connected...");
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
 };
 
 module.exports = connectDB;
